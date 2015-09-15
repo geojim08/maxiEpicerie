803902acdc1f64475514d5126fa05cd42553a4bc
geojim08 <geojim08@gmail.com>
2015-09-15 06:37:27 -0400

    First commit

---
+++ www/js/data.js
@@ -0,0 +1,144 @@
+var table_recette = [
+{
+"id" : 1,
+"nom" : "Pâtes au chèvre"
+},
+{
+"id" : 2,
+"nom" : "Tacos au tofu"
+}
+]
+
+var table_recette_ing = [
+{
+"id_recette" : 1,
+"id_ingredient" : 1
+},
+{
+"id_recette" : 1,
+"id_ingredient" : 2
+},
+{
+"id_recette" : 1,
+"id_ingredient" : 3
+},
+{
+"id_recette" : 1,
+"id_ingredient" : 4
+},
+{
+"id_recette" : 1,
+"id_ingredient" : 5
+},
+{
+"id_recette" : 1,
+"id_ingredient" : 6
+},
+{
+"id_recette" : 1,
+"id_ingredient" : 7
+},
+{
+"id_recette" : 1,
+"id_ingredient" : 8
+},
+{
+"id_recette" : 2,
+"id_ingredient" : 9
+},
+{
+"id_recette" : 2,
+"id_ingredient" : 4
+},
+{
+"id_recette" : 2,
+"id_ingredient" : 5
+},
+{
+"id_recette" : 2,
+"id_ingredient" : 10
+},
+{
+"id_recette" : 2,
+"id_ingredient" : 7
+},
+{
+"id_recette" : 2,
+"id_ingredient" : 11
+},
+{
+"id_recette" : 2,
+"id_ingredient" : 6
+},
+{
+"id_recette" : 2,
+"id_ingredient" : 12
+},
+{
+"id_recette" : 2,
+"id_ingredient" : 13
+},
+{
+"id_recette" : 2,
+"id_ingredient" : 14
+}
+]
+
+var table_ingredient = [
+{
+"id_ing" : 1,
+"nom" : "Pâtes soba"
+},
+{
+"id_ing" : 2,
+"nom" : "Pesto"
+},
+{
+"id_ing" : 3,
+"nom" : "Gousse d'ail"
+},
+{
+"id_ing" : 4,
+"nom" : "Huile d'olive"
+},
+{
+"id_ing" : 5,
+"nom" : "Oignons verts"
+},
+{
+"id_ing" : 6,
+"nom" : "Courgette"
+},
+{
+"id_ing" : 7,
+"nom" : "Tomates"
+},
+{
+"id_ing" : 8,
+"nom" : "Fromage de chèvre"
+},
+{
+"id_ing" : 9,
+"nom" : "Coquilles Tacos"
+},
+{
+"id_ing" : 10,
+"nom" : "Tofu"
+},
+{
+"id_ing" : 11,
+"nom" : "Champignons"
+},
+{
+"id_ing" : 12,
+"nom" : "Carotte"
+},
+{
+"id_ing" : 13,
+"nom" : "Graines de sésame"
+},
+{
+"id_ing" : 14,
+"nom" : "Tamari"
+}
+]
